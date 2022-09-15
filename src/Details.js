3e1b3a28bb1e609e9d0ade7c28305224f477a367
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-09-15 22:13:42 +0900

    Added the carousel and clicable

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,15 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = { loading: true };
-  }
+  //   this.state = { loading: true };
+  // }
+
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
@@ -21,10 +24,12 @@ class Details extends Component {
     if (this.state.loading) {
       return <h2>loading...</h2>;
     }
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
